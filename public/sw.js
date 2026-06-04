3ed8d6edbc4948cb3ca07c6d04ca6ea196e533ae
Troy <bar.troy2011@gmail.com>
2026-06-04 13:51:03 +0900

    Use icon 060 for companions tab

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'troy-app-v20260604a';
+const CACHE_VERSION = 'troy-app-v20260604b';
 const CORE_CACHE = `troy-core-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `troy-runtime-${CACHE_VERSION}`;
 
