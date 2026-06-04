908299aa3ad130d4637f22ef1cfb07eccba479b9
Troy <bar.troy2011@gmail.com>
2026-06-04 16:46:09 +0900

    Center inventory item artwork in panels

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'troy-app-v20260604g';
+const CACHE_VERSION = 'troy-app-v20260604h';
 const CORE_CACHE = `troy-core-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `troy-runtime-${CACHE_VERSION}`;
 
