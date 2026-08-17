3f60461a7818055d44b57a2bd9bc932b2649a387
Troy <bar.troy2011@gmail.com>
2026-08-17 16:47:49 +0900

    Adjust stage-02 battlefield background offset to -50px

--- public/js/tarotKingdomBattlefields.js
+++ public/js/tarotKingdomBattlefields.js
@@ -14,8 +14,8 @@ const BATTLEFIELD_DEFINITIONS = [
     surface: 'ship-deck',
     shipSide: true,
     groundStartPercent: 46,
-    backgroundPosition: 'center -28px',
-    backgroundSize: '100% calc(100% + 28px)'
+    backgroundPosition: 'center -50px',
+    backgroundSize: '100% calc(100% + 50px)'
   },
   {
     id: 'stage-03-island-causeway',
